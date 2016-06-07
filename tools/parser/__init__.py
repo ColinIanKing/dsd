7617357ebda2c287e2af858273256cf1c017b692
Al Stone <ahs3@redhat.com>
2016-06-06 22:26:24 -0600

    Had to rethink parser, refactor it to be cleaner
    Signed-off-by: Al Stone <ahs3@redhat.com>

--- tools/parser/__init__.py
+++ tools/parser/__init__.py
@@ -32,7 +32,7 @@ SUBPACKAGE      = "subpackage"
 TOKEN           = "token"
 TYPE            = "type"
 USAGE           = "usage"
-VALUE           = "value"
+VALUES          = "values"
 VENDOR          = "vendor"
 
 # tokens for bus
@@ -42,12 +42,14 @@ SHARED          = "shared"
 ABSTRACT        = "abstract"
 DEFINITION      = "definition"
 SUBSET          = "subset"
+SET_TYPES       = [ ABSTRACT, DEFINITION, SUBSET ]
 
 # tokens for type values (INTEGER, REFERENCE can be keywords, in context)
 INTEGER         = "integer"
 PACKAGE         = "package"
 REFERENCE       = "reference"
 STRING          = "string"
+VALUE_TYPES     = [ INTEGER, PACKAGE, REFERENCE, STRING ]
 
 # tokens for usage values
 OPTIONAL        = "optional"
@@ -55,13 +57,18 @@ REQUIRED        = "required"
 
 # misc tokens
 NAME            = "name"
+RANGE           = "range"
 
 # reserved word token list
 KEYWORDS = [ ACKED_BY, BUS, DESCRIPTION, DERIVED_FROM, DEVICE_ID, 
              EXAMPLE, PROPERTY, PROPERTY_SET, REQUIRES, REVIEWED_BY,
              REVISION, SET_TYPE, SUBMITTED_BY, SUBPACKAGE, TOKEN, 
-             TYPE, USAGE, VALUE, VENDOR,
+             TYPE, USAGE, VALUES, VENDOR,
            ]
+SET_KEYWORDS = [ ACKED_BY, BUS, DESCRIPTION, DERIVED_FROM, DEVICE_ID, 
+                 EXAMPLE, PROPERTY, PROPERTY_SET, REQUIRES, REVIEWED_BY,
+                 REVISION, SET_TYPE, SUBMITTED_BY, VENDOR,
+               ]
 
 class Parser:
     def __init__(self, fname):
@@ -90,18 +97,76 @@ class Parser:
         print("+------------------------------------------------------+")
         print("| File: %s" % (self.fname))
         print("+------------------------------------------------------+")
-        
         for ii in self.text:
             print("%s" % (ii), end='')
 
+        print("\n")
+        print("+------------------------------------------------------+")
+        print("| Attributes                                           +")
         print("+------------------------------------------------------+")
         print(self.attrs)
-        print(self.properties)
+
+        print("\n")
+        print("+------------------------------------------------------+")
+        print("| Properties                                           +")
+        print("+------------------------------------------------------+")
+        for ii in self.properties.keys():
+            print("==== %s ====" % (ii))
+            print(self.properties)
 
         return
 
     def has_errors(self):
         return self.error
+
+    def __look_for_colon(self, attr, tok, lexer, errcnt, ok_state):
+        errflag = False
+        if tok == COLON:
+            state = ok_state
+        else:
+            errflag = True
+            errcnt += 1
+            state = 0
+            print("%scolon required after %s" % (lexer.error_leader(), attr))
+        return (errflag, errcnt, state)
+
+    def __get_raw_oneline(self, lexer, tok):
+        text = ""
+        while tok != EOL:
+            text += tok + " "
+            tok = lexer.get_token()
+            if tok in KEYWORDS:
+                lexer.push_token(tok)
+                break
+        print("=> one line text/%d: %s" % (len(text.split("\n")), text.strip()))
+        return text.strip()
+
+    def __get_raw_multiline(self, lexer, tok):
+        text = ""
+        if tok != EOL:
+            text = tok + " "
+        state = 0
+        c = lexer.instream.read(1)
+        while True:
+            if state == 0:
+                if c == "\n":
+                    state = 1
+                else:
+                    text += c
+                    c = lexer.instream.read(1)
+            
+            elif state == 1:
+                tok = lexer.get_token()
+                if tok in KEYWORDS:
+                    lexer.push_token(tok)
+                    break
+                else:
+                    text += " " + tok
+                    state = 0
+
+        lexer.lineno += 1
+        print("=> text/%d: %s" % (len(text.split("\n")), text.strip()))
+        return text.strip()
 
     def parse(self):
         linenum = 0
@@ -110,6 +175,7 @@ class Parser:
         lexer.infile = self.fname
         lexer.wordchars = lexer.wordchars + '-./'
         lexer.whitespace = " \t"
+        lexer.quotes = ""
         self.error = False
         tok = lexer.get_token()
         state = 0
@@ -129,81 +195,179 @@ class Parser:
                           (lexer.error_leader(), PROPERTY_SET))
 
             elif state == 1:
-                if tok == COLON:
-                    state = 2
-                else:
-                    self.error = True
-                    self.errors += 1
-                    print("%scolon required after %s" %
-                          (lexer.error_leader(), attr))
+                self.error, self.errors, state = \
+                    self.__look_for_colon(attr, tok, lexer, self.errors, 2)
 
             elif state == 2:
-                if len(tok) > 0:
-                    if attr not in self.attrs.keys():
-                        self.attrs[attr] = {}
-                    self.attrs[attr] = tok
-                    state = 3
+                text = self.__get_raw_oneline(lexer, tok)
+                if attr not in self.attrs.keys():
+                    self.attrs[attr] = {}
+                self.attrs[attr] = text
+                state = 3
 
             elif state == 3:
-                if tok != EOL:
-                    if attr not in self.attrs.keys():
-                        self.attrs[attr] = {}
-                    self.attrs[attr] += ' ' + tok
+                if tok == EOL:
                     state = 3
-                else:
+                elif tok in SET_KEYWORDS:
+                    attr = tok
                     state = 4
 
             elif state == 4:
-                if tok == EOL:
-                    state = 4
-                elif tok == DESCRIPTION:
-                    attr = tok
-                    state = 5
-                elif tok == PROPERTY:
-                    attr = tok
-                    state = 7
-                elif tok in KEYWORDS:
-                    attr = tok
-                    state = 1
-                else:
-                    self.error = True
-                    self.errors += 1
-                    print("%skeyword expected instead of %s" %
-                          (lexer.error_leader(), tok))
+                self.error, self.errors, state = \
+                    self.__look_for_colon(attr, tok, lexer, self.errors, 5)
 
             elif state == 5:
-                if tok == COLON:
-                    state = 5
-                if tok == EOL:
-                    state = 6
-                else:
-                    if prop not in self.properties.keys():
-                        self.properties[prop] = {}
-                    self.properties[prop][DESCRIPTION] = tok
+                if attr not in self.attrs.keys():
+                    self.attrs[attr] = {}
 
-            elif state == 6:
-                if tok == EOL:
-                    state = 4
-                else:
-                    if prop not in self.properties.keys():
-                        self.properties[prop] = {}
-                    self.properties[prop][DESCRIPTION] += " " + tok
+                if attr == SET_TYPE:
+                    if tok in SET_TYPES:
+                        self.attrs[attr] = tok
+                        state = 3
+                    else:
+                        self.error = True
+                        self.errors += 1
+                        print("%sundefined property-set type: %s" %
+                            (lexer.error_leader(), tok))
 
-            elif state == 7:
-                if tok == COLON:
-                    state = 8
-                else:
-                    self.error = True
-                    self.errors += 1
-                    print("%scolon required after %s" %
-                          (lexer.error_leader(), attr))
+                elif attr == DERIVED_FROM or attr == ACKED_BY or \
+                     attr == SUBMITTED_BY or attr == REVIEWED_BY:
+                    if len(self.attrs[attr]) < 1:
+                        self.attrs[attr] = []
+                    text = self.__get_raw_oneline(lexer, tok)
+                    self.attrs[attr].append(text.strip())
+                    state = 3
 
-            elif state == 8:
-                prop = tok
-                if prop not in self.properties.keys():
-                    self.properties[prop] = {}
-                self.properties[prop][NAME] = prop 
-                state = 4
+                elif attr == VENDOR or attr == BUS or attr == DEVICE_ID or \
+                     attr == REVISION:
+                    text = self.__get_raw_oneline(lexer, tok)
+                    self.attrs[attr] = text
+                    state = 3
+
+#           elif state == 7:
+#               self.error, self.errors, state = \
+#                   self.__look_for_colon(attr, tok, lexer, self.errors, 8)
+
+#           elif state == 8:
+#               prop = tok
+#               if prop not in self.properties.keys():
+#                   self.properties[prop] = {}
+#               self.properties[prop][NAME] = prop 
+#               state = 4
+
+#           elif state == 9:
+#               self.error, self.errors, state = \
+#                   self.__look_for_colon(attr, tok, lexer, self.errors, 10)
+
+#           elif state == 10:
+#               if prop not in self.properties.keys():
+#                   self.properties[prop] = {}
+#               self.properties[prop][TYPE] = tok 
+#               prop_type = tok
+#               state = 4
+
+#           elif state == 11:
+#               self.error, self.errors, state = \
+#                   self.__look_for_colon(attr, tok, lexer, self.errors, 12)
+#               
+#           elif state == 12:
+#               if tok == EOL:
+#                   state = 13
+#               elif tok != EOL:
+#                   self.error = True
+#                   self.errors += 1
+#                   print("%sEOL expected after %s" %
+#                         (lexer.error_leader(), attr))
+
+#           elif state == 13:
+#               if tok in KEYWORDS:
+#                   state = 4
+#                   continue
+#               value = {}
+#               value[TYPE] = tok
+#               if prop_type == STRING:
+#                   if tok == TOKEN:
+#                       state = 14
+#                   else:
+#                       self.error = True
+#                       self.errors += 1
+#                       print("%sexpected the 'token' keyword" %
+#                             (lexer.error_leader()))
+#               elif prop_type == INTEGER:
+#                   if tok == INTEGER:
+#                       state = 15
+#                   else:
+#                       self.error = True
+#                       self.errors += 1
+#                       print("%sexpected the 'integer' keyword" %
+#                             (lexer.error_leader()))
+#               elif prop_type == REFERENCE:
+#                   if tok == REFERENCE:
+#                       state = 16
+#                   else:
+#                       self.error = True
+#                       self.errors += 1
+#                       print("%sexpected the 'reference' keyword" %
+#                             (lexer.error_leader()))
+#               elif prop_type == PACKAGE:
+#                   if tok == SUBPACKAGE:
+#                       state = 17
+#                   else:
+#                       self.error = True
+#                       self.errors += 1
+#                       print("%sexpected the 'subpackage' keyword" %
+#                             (lexer.error_leader()))
+#               else:
+#                   self.error = True
+#                   self.errors += 1
+#                   print("%sunknown value type: %s" %
+#                         (lexer.error_leader(), prop_type))
+
+#           elif state == 14:
+#               self.error, self.errors, state = \
+#                   self.__look_for_colon(attr, tok, lexer, self.errors, 15)
+
+#           elif state == 15:
+#               if tok == EOL:
+#                   self.error = True
+#                   self.errors += 1
+#                   print("%sexpected but did not find a value" %
+#                         (lexer.error_leader()))
+#               else:
+#                   value[RANGE] = tok
+#                   state = 16
+
+#           elif state == 16:
+#               if tok != EOL:
+#                   value[RANGE] += " " + tok
+#                   state = 16
+#               else:
+#                   state = 17
+
+#           elif state == 17:
+#               if tok == DESCRIPTION:
+#                   state = 18
+#               else:
+#                   self.error = True
+#                   self.errors += 1
+#                   print("%sexpected but did not find 'description'" %
+#                         (lexer.error_leader()))
+#               
+#           elif state == 18:
+#               self.error, self.errors, state = \
+#                   self.__look_for_colon(attr, tok, lexer, self.errors, 19)
+
+#           elif state == 19:
+#               if tok == EOL:
+#                   self.error = True
+#                   self.errors += 1
+#                   print("%sexpected but did not find a description" %
+#                         (lexer.error_leader()))
+#               else:
+#                   text = self.__get_raw_text(lexer, tok)
+#                   value[DESCRIPTION] = text
+#                   self.properties[prop][VALUES].append(value)
+#                   state = 13
 
             else:
                 print("? internal error: state %d, token %s" % (state, tok))
